68349c260a9c25190cfc20cf0569e04523d09c1c
Gabriele Provenzano <146100399+gabrieleprovenzano@users.noreply.github.com>
2024-02-19 09:09:10 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { HelloWorld } from "./HelloWorld";
+import { Message } from "./Message";
 
 export function App() {
     return(
         <div>
             <HelloWorld />
+            <Message />
         </div>
     )
 }
